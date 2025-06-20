aaf3f3bbc9184e191cafad5691ba76475eb8f51a
Vitaly Perevoshchikov <vitaly.perev@gmail.com>
2025-06-20 19:40:44 +0200

    Initialize SpringBoot project

---
+++ src/test/java/com/perevos/fintra/FintraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perevos.fintra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
