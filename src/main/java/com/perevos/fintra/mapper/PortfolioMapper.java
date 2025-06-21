a3fe0b29929aa7ec7a41a3f8e7285a9ca5277b85
Vitaly Perevoshchikov <vitaly.perev@gmail.com>
2025-06-21 02:52:21 +0200

    Add method to PortfolioMapper which maps a portfolio entity into PortfolioDetailsDto

--- src/main/java/com/perevos/fintra/mapper/PortfolioMapper.java
+++ src/main/java/com/perevos/fintra/mapper/PortfolioMapper.java
@@ -1,17 +1,36 @@
 package com.perevos.fintra.mapper;
 
+import com.perevos.fintra.dto.PortfolioDetailsDto;
 import com.perevos.fintra.dto.PortfolioOverviewDto;
 import com.perevos.fintra.entity.Portfolio;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PortfolioMapper {
 
-    public PortfolioOverviewDto toDto(Portfolio portfolio) {
+    private final PortfolioAssetMapper assetMapper;
+
+    public PortfolioOverviewDto toPortfolioOverviewDto(Portfolio portfolio) {
         return PortfolioOverviewDto.builder()
                 .id(portfolio.getId())
                 .name(portfolio.getName())
                 .description(portfolio.getDescription())
                 .build();
     }
+
+    public PortfolioDetailsDto toPortfolioDetailsDto(Portfolio portfolio) {
+        return PortfolioDetailsDto.builder()
+                .id(portfolio.getId())
+                .name(portfolio.getName())
+                .description(portfolio.getDescription())
+                .assets(portfolio.getAssets()
+                        .stream()
+                        .map(assetMapper::toPortfolioAssetDto)
+                        .toList())
+                .build();
+    }
+
+
 }
--- src/test/java/com/perevos/fintra/mapper/PortfolioMapperTest.java
+++ src/test/java/com/perevos/fintra/mapper/PortfolioMapperTest.java
@@ -1,23 +1,44 @@
 package com.perevos.fintra.mapper;
 
+import com.perevos.fintra.dto.PortfolioDetailsDto;
 import com.perevos.fintra.dto.PortfolioOverviewDto;
 import com.perevos.fintra.entity.Portfolio;
+import com.perevos.fintra.entity.PortfolioAsset;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class PortfolioMapperTest {
 
-    private final PortfolioMapper portfolioMapper = new PortfolioMapper();
+    private static final long PORTFOLIO_ID = 1L;
+    private static final String PORTFOLIO_NAME = "Test Portfolio";
+    private static final String PORTFOLIO_DESCRIPTION = "Sample description";
+    private static final String ASSET_TICKER_1 = "AAPL";
+    private static final String ASSET_TICKER_2 = "AMZN";
+
+    @Mock
+    private PortfolioAssetMapper assetMapper;
+
+    @InjectMocks
+    private PortfolioMapper portfolioMapper;
 
     @Test
     void toPortfolioOverviewDto_shouldMapToDtoCorrectly() {
         // arrange
         Portfolio portfolio = Portfolio.builder()
-                .id(1L)
-                .name("Test Portfolio")
-                .description("Sample description")
+                .id(PORTFOLIO_ID)
+                .name(PORTFOLIO_NAME)
+                .description(PORTFOLIO_DESCRIPTION)
                 .build();
 
         // act
@@ -25,8 +46,38 @@ public class PortfolioMapperTest {
 
         // assert
         assertNotNull(dto);
-        assertEquals(1L, dto.getId());
-        assertEquals("Test Portfolio", dto.getName());
-        assertEquals("Sample description", dto.getDescription());
+        assertEquals(PORTFOLIO_ID, dto.getId());
+        assertEquals(PORTFOLIO_NAME, dto.getName());
+        assertEquals(PORTFOLIO_DESCRIPTION, dto.getDescription());
     }
+
+    @Test
+    void toPortfolioDetailsDto_shouldMapToDtoCorrectly() {
+        // arrange
+        PortfolioAsset asset1 = PortfolioAsset.builder().ticker(ASSET_TICKER_1).build();
+        PortfolioAsset asset2 = PortfolioAsset.builder().ticker(ASSET_TICKER_2).build();
+        Portfolio portfolio = Portfolio.builder()
+                .id(PORTFOLIO_ID)
+                .name(PORTFOLIO_NAME)
+                .description(PORTFOLIO_DESCRIPTION)
+                .assets(List.of(
+                        asset1,
+                        asset2
+                ))
+                .build();
+
+        // act
+        PortfolioDetailsDto dto = portfolioMapper.toPortfolioDetailsDto(portfolio);
+
+        // assert
+        assertNotNull(dto);
+        assertEquals(PORTFOLIO_ID, dto.getId());
+        assertEquals(PORTFOLIO_NAME, dto.getName());
+        assertEquals(PORTFOLIO_DESCRIPTION, dto.getDescription());
+        assertEquals(2, dto.getAssets().size());
+
+        verify(assetMapper, times(1)).toPortfolioAssetDto(asset1);
+        verify(assetMapper, times(1)).toPortfolioAssetDto(asset2);
+    }
+
 }
