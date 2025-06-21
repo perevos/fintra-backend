cdc7f132a26d4d2418c120c8881817e043d4ce7d
Vitaly Perevoshchikov <vitaly.perev@gmail.com>
2025-06-21 19:44:20 +0200

    Add dummy assets to portfolios in loadDummyPortfolios method

--- src/main/java/com/perevos/fintra/data/DataLoaderConfig.java
+++ src/main/java/com/perevos/fintra/data/DataLoaderConfig.java
@@ -1,6 +1,7 @@
 package com.perevos.fintra.data;
 
 import com.perevos.fintra.entity.Portfolio;
+import com.perevos.fintra.entity.PortfolioAsset;
 import com.perevos.fintra.repository.PortfolioRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -21,21 +22,65 @@ public class DataLoaderConfig {
     public CommandLineRunner loadDummyPortfolios(PortfolioRepository portfolioRepository) {
         return args -> {
             if (portfolioRepository.count() == 0) {
-                portfolioRepository.save(Portfolio.builder()
-                                .name("Growth portfolio")
-                                .description("Focused on high growth stocks")
-                        .build());
-
-                portfolioRepository.save(Portfolio.builder()
-                                .name("Income Portfolio")
-                                .description("Designed for steady dividend income")
-                        .build());
-
-                portfolioRepository.save(Portfolio.builder()
-                                .name("Balanced Portfolio")
-                                .description("Mix of growth and income investments")
-                        .build());
+                portfolioRepository.save(getGrowthPortfolio());
+                portfolioRepository.save(getIncomePortfolio());
+                portfolioRepository.save(getBalancedPortfolio());
             }
         };
+    }
+
+    private Portfolio getGrowthPortfolio() {
+        Portfolio growthPortfolio = Portfolio.builder()
+                .name("Growth portfolio")
+                .description("Focused on high growth stocks")
+                .build();
+
+        growthPortfolio.getAssets().add(PortfolioAsset.builder()
+                .ticker("AAPL")
+                .quantity(10.0)
+                .purchasePrice(190.3)
+                .portfolio(growthPortfolio)
+                .build());
+
+        return growthPortfolio;
+    }
+
+    private Portfolio getIncomePortfolio() {
+        Portfolio incomePortfolio = Portfolio.builder()
+                .name("Income Portfolio")
+                .description("Designed for steady dividend income")
+                .build();
+
+        incomePortfolio.getAssets().add(PortfolioAsset.builder()
+                .ticker("JNJ")
+                .quantity(20.0)
+                .purchasePrice(149.99)
+                .portfolio(incomePortfolio)
+                .build());
+
+        return incomePortfolio;
+    }
+
+    private Portfolio getBalancedPortfolio() {
+        Portfolio balancedPortfolio = Portfolio.builder()
+                .name("Balanced Portfolio")
+                .description("Mix of growth and income investments")
+                .build();
+
+        balancedPortfolio.getAssets().add(PortfolioAsset.builder()
+                .ticker("AAPL")
+                .quantity(5.0)
+                .purchasePrice(190.3)
+                .portfolio(balancedPortfolio)
+                .build());
+
+        balancedPortfolio.getAssets().add(PortfolioAsset.builder()
+                .ticker("JNJ")
+                .quantity(6.0)
+                .purchasePrice(149.99)
+                .portfolio(balancedPortfolio)
+                .build());
+
+        return balancedPortfolio;
     }
 }
