c7458c7c77284ec061911f9fcf30e3602d55c255
Vitaly Perevoshchikov <vitaly.perev@gmail.com>
2025-06-21 02:00:59 +0200

    Add a PortfolioAsset entity and establish a bidirectional link between this and the Portfolio entity

--- src/main/java/com/perevos/fintra/entity/PortfolioAsset.java
+++ src/main/java/com/perevos/fintra/entity/PortfolioAsset.java
@@ -1,11 +1,12 @@
 package com.perevos.fintra.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,27 +14,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Table(name = "portfolios")
+@Table(name = "portfolio_assets")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Portfolio {
+public class PortfolioAsset {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String ticker;
 
-    private String description;
+    private Double quantity;
 
-    @Builder.Default
-    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PortfolioAsset> assets = new ArrayList<>();
+    private Double purchasePrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private Portfolio portfolio;
 }
